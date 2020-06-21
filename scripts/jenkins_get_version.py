394b457cccde14791dd56c81ee12427be977cc76
korzeniowska18 <korzeniowska.natalia18@users.noreply.github.com>
2020-06-21 13:13:41 +0200

    update

--- scripts/jenkins_get_version.py
+++ scripts/jenkins_get_version.py
@@ -2,9 +2,9 @@ import jenkins
 
 server = jenkins.Jenkins('http://localhost:8080',
                          username='admin',
-                         password='')  # place password here
+                         password='92046d8a89df4e009962a2f51ce484dc')  # place password here
 
 
 user = server.get_whoami()
 version = server.get_version()
-print('Hello %s from Jenkins %s' % (user['fullName'], version))
+print('Hello %s from Jenkins %s' % (user['Natalia'], version))
